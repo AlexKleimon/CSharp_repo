96b0f0b13968a4fa2af99a1f05079e5f0661e14e
Alex <Utin97@mail.ru>
2022-11-29 19:55:56 +0300

    addid elsehello

---
+++ Example4_ElseHello/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+Console.Write("Введите имя пользователя: ");
+string UserName = Console.ReadLine();
+
+if(UserName.ToLower()=="маша")
+{
+    Console.WriteLine("ПРИВЕТ МАША!!!)))))");
+}
+else
+{
+    Console.WriteLine("Привет {0}!", UserName);
+}
